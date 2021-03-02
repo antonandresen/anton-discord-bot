2f8755c954b0d40667bc126e9908c97e77a300e6
Anton <anton_real_madrid@icloud.com>
2021-03-02 10:00:07 +0100

    fix stop command

--- src/commands/music/stop.js
+++ src/commands/music/stop.js
@@ -7,20 +7,25 @@ module.exports = {
   usage: '[command name]',
   cooldown: 2,
   async execute(message, args) {
+    const voiceData = message.client.activeVoice.get(message.guild.id) || {};
+    console.log({ voiceData });
+
     if (!message.member.voice)
       return message.channel.send(
         'You have to be connected to the voice chat to stop the bot.'
       );
 
-    if (!message.guild.me.voiceChannel)
+    if (!voiceData.connection)
       return message.channel.send('The bot is not playing music.');
 
-    if (message.guild.me.voiceChannelID !== message.member.voiceChannelID)
+    /*if (message.guild.me.voiceChannelID !== message.member.voiceChannelID)
       return message.channel.send(
         'You have to be connected to the voice chat to stop the bot.'
-      );
+      );*/
 
-    message.guild.me.voiceChannel.leave();
+    message.client.activeVoice.delete(message.guild.id);
+    const vc = voiceData.connection;
+    if (vc) await vc.disconnect();
     message.channel.send('Stopping the music...');
   },
 };
