e199b77eb364b61f35b67679f69ac0be7c87a2b6
Anton <anton_real_madrid@icloud.com>
2021-06-27 17:54:06 +0200

    månsson event

--- src/events/guild/messageUpdate.js
+++ src/events/guild/messageUpdate.js
@@ -1,3 +1,20 @@
+const constants = require('../../constants');
+
 module.exports = (oldMessage, newMessage) => {
   console.log('messageUpdate:', newMessage);
+
+  // MÅNSSON CHECKS
+  if (newMessage.author.id === '136919142174294016') {
+    console.log('MÅNSSON TYPED: ', newMessage.content);
+    if (
+      constants.MANSSON_BANNED_SEQUENCES.find((seq) =>
+        newMessage.content.includes(seq)
+      )
+    ) {
+      await newMessage.delete();
+      return newMessage.reply(
+        'Njaaa månsson, dedär vet jag inte om jag riktigt går med på :))'
+      );
+    }
+  }
 };
